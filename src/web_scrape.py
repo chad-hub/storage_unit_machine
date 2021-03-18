e25941b54a2d0c0047b556e9a6892d95fef5882a
Chad Hutchison <chad.b.hutchison@gmail.com>
2021-03-18 15:33:07 -0500

    Got info into dataframe

--- src/web_scrape.py
+++ src/web_scrape.py
@@ -32,7 +32,6 @@ def get_summary(url, zipcodes):
 # %%
 
 def scrape_data(url, url2, zipcodes, num_units):
-  # searches = []
   for z in zipcodes:
     sub_links = []
     search_url = url + str(z) + url2
@@ -45,20 +44,78 @@ def scrape_data(url, url2, zipcodes, num_units):
       link = i.get('href')
       sub_links.append(link)
   sub_links = sub_links[2:num_units+2]
-  for s in sub_links:
-    new_link = url[:26] + s
-    sub_req = urllib.request.Request(new_link, headers={'User-Agent' : 'Magic Browser'})
-    sub_response = urllib.request.urlopen( sub_req )
-    sub_html = sub_response.read()
-    sub_soup = BeautifulSoup(sub_html, 'html.parser')
+  return sub_links
 
 
-  # searches.append(sub_links)
+  # %%
+  def scrape_details(url, subs):
+    output = []
+    for idx, s in enumerate(sub_links):
+      d = {}
+      new_link = url[:25] + s
 
-  print(len(sub_links))
-  for i in sub_links:
-    print(i)
+      req = urllib.request.Request(new_link, headers={'User-Agent' : 'Magic Browser'})
+      response = urllib.request.urlopen( req )
+      html = response.read()
+      soup = BeautifulSoup(html, 'html.parser')
 
+      name = soup.find('h1', class_ = "facility-name sf-type sf-type-large").get_text()
+      name = name.split('-')[0]
+      d['name'] = name
+
+      address = soup.find('h2',  class_ = "facility-address sf-type").get_text()
+      d['address'] = address
+
+      city = soup.find('h2',  class_ = "facility-address sf-type").get_text()
+      city = city.split(' ')[-3].strip(',')
+      d['city'] = city
+
+      state = soup.find('h2',  class_ = "facility-address sf-type").get_text()
+      state = state.split(' ')[-2]
+      d['state'] = state
+
+      zipcode = soup.find('h2',  class_ = "facility-address sf-type").get_text()
+      zipcode = zipcode.split(' ')[-1]
+      d['zipcode'] = zipcode
+
+      phone = soup.find('span', class_ = "number").get_text()
+      d['phone'] = phone
+
+      try:
+        rating = soup.find('span', class_ = "sf-type sf-type-tiny").get_text()
+        rating = rating.split(' ')[0]
+      except AttributeError:
+        rating = 'N/A'
+      d['rating'] = rating
+
+      try:
+        reviews = soup.find('span' , class_ = "review-count sf-type").get_text()
+        reviews = reviews.split(' ')[-2]
+      except AttributeError:
+        reviews = 'N/A'
+      d['reviews'] = reviews
+
+      amenities = soup.findAll('div', class_ = "amenity")
+      for idx, i in enumerate(amenities):
+        amm = i.get_text()
+        if i.svg['class'][1] == 'checkmark':
+          d[amm] = 1
+        else:
+          d[amm] = 0
+      output.append(d)
+    print(idx)
+    time.sleep(random.randint(1,6))
+    df = pd.DataFrame(output)
+    return df
+
+
+
+
+
+
+
+
+# %%
 
 
 
@@ -70,11 +127,14 @@ if __name__ == '__main__':
   url = 'https://www.sparefoot.com/search.html?location='
   num_units, table = get_summary(url, zipcode)
   if num_units > 100:
-    num_units = 100
-
+    num_units = 10
   url2 = '&searchType=storage&page=1&listingsPerPage=' + str(num_units)
-  table
-  # scrape_data(url, url2, zipcode, num_units)
+
+  sub_links = scrape_data(url, url2, zipcode, num_units)
+
+  df = scrape_details(url, sub_links)
+  df.head()
+
 
 
 # %%
